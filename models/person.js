38057aeb38948208241015629278a1189924810b
teepiik <teemu.piik@gmail.com>
2018-02-11 18:50:41 +0200

    heroku

--- models/person.js
+++ models/person.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 // korvaa url oman tietokantasi urlilla. ethän laita salasanaa Githubiin!
+
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url);
