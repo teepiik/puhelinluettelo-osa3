2fbf38c376305fb240c2e5c246eed708866315bc
teepiik <teemu.piik@gmail.com>
2018-02-11 16:57:18 +0200

    t.3.16-3.18 tehty

--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // korvaa url oman tietokantasi urlilla. ethän laita salasanaa Githubiin!
 const url =
-  "mongodb://person:passupoistettu@ds227858.mlab.com:27858/phonebook-fullstack";
+  "";
 
 mongoose.connect(url);
 
